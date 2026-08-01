a3f5e6b21f41f28911326ea37ab2555f5c523f0e
Riya <mahrajriya@gmail.com>
2026-08-01 11:14:29 -0700

    Error correction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { ConnectDB } from "./config/db.config.js";
-import userRotes from "./config/routes/user.routes.js"
+import userRoutes from "./config/routes/user.routes.js";
 
 dotenv.config();
 
