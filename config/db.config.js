2ca336108c5b66cb7d84154c85124c7fcfee389c
Riya <mahrajriya@gmail.com>
2026-08-01 10:45:21 -0700

    Connect Database

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 
-export  const connectDB = async () => {
+export  const ConnectDB = async () => {
     try {
-
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("DATABASE CONNECTED")
     } catch (error) {
-        
+        console.log((error.message))
+        throw new Error("Sometimes went ", error)
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { ConnectDB } from "./config/db.config.js";
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 
 const app = express();
+ConnectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
