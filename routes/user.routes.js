c43fbf85f62e84b617c406c4e15affe301ee0a56
Riya <mahrajriya@gmail.com>
2026-08-02 11:12:45 -0700

    Configure Cloudinary and update user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import user from "../models/user.models.js";
+
 
 
 const router = express.Router();
 
-router.post("/singup" , (req, res) => {
-    res.send("User singup route");
+router.post("/singup" , async(req, res) => {
+    try {
+        const hashcode = await bcrypt.hash(req.body.password , 10);
+        
+    } catch (error) {
+        
+    }
 })
 
 export default router;
