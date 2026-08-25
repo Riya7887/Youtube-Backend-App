2301fb84440cf9036a10497fcb6d1bfab16248c0
Riya <mahrajriya@gmail.com>
2026-08-25 09:19:27 -0700

    Created video models

---
+++ models/video.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const videoschema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    videoUrl:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    thumbnailUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    thumbnailId: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    tags:{
+        type: String,
+        trim: true,
+    },
+    likes:{
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+    dislikes:{
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+    views:{
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+    likedBy:[{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    dislikedBy:[{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    viewedBy:[{type: mogoose.Schema.Types.objectId, ref: "User"}],
+     
+}, {timestamps: true});
+
+const videoModel = mongoose.model("video", videoschema);
+export default videoModel;
